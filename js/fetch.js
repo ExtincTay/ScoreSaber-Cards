5dd2fa2a82b9e35f067eacbce27e4bd35d45b29d
BuildTools <unconfigured@null.spigotmc.org>
2022-09-19 18:08:04 +0100

    made acc have 2 decimals and a percentage symbol

--- js/fetch.js
+++ js/fetch.js
@@ -4,8 +4,8 @@ async function getScores() {
   const playerName = json.playerInfo.playerName
   const playerNameNoQuotes = playerName.replace(/"/g, "");
   let textaccavg = json.scoreStats.averageRankedAccuracy;
-  let resultaccavg = textaccavg.toString().slice(0, 2);
+  let resultaccavg = textaccavg.toString().slice(0, 5);
   document.getElementById("rank").innerHTML += `<pre>${JSON.stringify(json.playerInfo.rank, null, 2)}</pre>`;
   document.getElementById("name").innerHTML += playerNameNoQuotes;
-  document.getElementById("Acc").innerHTML += resultaccavg;
+  document.getElementById("Acc").innerHTML += resultaccavg + "%";
 }
