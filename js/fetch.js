2ba375e3e421465c60bddeeb02b1dfe0f2e062f7
BuildTools <unconfigured@null.spigotmc.org>
2022-09-19 17:34:24 +0100

    chopped acc to no decimals

--- js/fetch.js
+++ js/fetch.js
@@ -3,7 +3,9 @@ async function getScores() {
   const json = await resp.json();
   const playerName = json.playerInfo.playerName
   const playerNameNoQuotes = playerName.replace(/"/g, "");
+  let textacc = json.scoreStats.averageRankedAccuracy;
+  let resultacc = textacc.slice(0, 2);
   document.getElementById("rank").innerHTML += `<pre>${JSON.stringify(json.playerInfo.rank, null, 2)}</pre>`;
   document.getElementById("name").innerHTML += playerNameNoQuotes;
-	document.getElementById("Acc").innerHTML += `<pre>${JSON.stringify(json.scoreStats.averageRankedAccuracy, null, 2)}</pre>`;
+	document.getElementById("Acc").innerHTML += `<pre>${JSON.stringify(resultacc, null, 2)}</pre>`;
 }
